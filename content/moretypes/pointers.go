65cb1dcc3086ecfe2a4e372b1a2f91a0a475de89
Красимир Беров <k.berov@gmail.com>
2017-09-12 23:14:42 +0200

    До "Указатели към структури"

--- content/moretypes/pointers.go
+++ content/moretypes/pointers.go
@@ -7,12 +7,12 @@ import "fmt"
 func main() {
 	i, j := 42, 2701
 
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
+	p := &i         // p сочи към i
+	fmt.Println(*p) // прочита се i през указателя
+	*p = 21         // задава се i през указателя
+	fmt.Println(i)  // показва новата стойност на i
 
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+	p = &j         // p сочи към j
+	*p = *p / 37   // деление на j чрез указателя
+	fmt.Println(j) // показва новата стойност на j
 }
